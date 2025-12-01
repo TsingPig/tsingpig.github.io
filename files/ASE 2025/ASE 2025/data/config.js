60dade5883c21e94f07d95dc5072d819d2ff63d5
Zhengyang Zhu <1114196607@qq.com>
2025-12-01 18:51:41 +0800

    upd: papers

---
+++ files/ASE 2025/ASE 2025/data/config.js
@@ -0,0 +1,18 @@
+var config = {
+	"proceeding": {
+		"acronym": "ASE",
+		"year": 2025
+	},
+	"bannerFile": "content/images/ASE 2025 Banner.jpg",
+	"enableGDPR": false,
+	"enableAffiliationIndex": false,
+	"videoPlayer": "mediaelement",
+	"webpubDownload": {
+		"enabled": null,
+		"location": "pdfs/ASE 2025.zip"
+	},
+	"services": {
+		"authEnabled": false,
+		"trackingEnabled": false
+	}
+};
